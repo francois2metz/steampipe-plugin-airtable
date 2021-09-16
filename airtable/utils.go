eaf7bcf6ea0221fc3f7c222fc87e9b8fa06e8941
François de Metz <francois@2metz.fr>
2021-09-16 16:03:42 +0200

    Cache the airtable client.

--- airtable/utils.go
+++ airtable/utils.go
@@ -9,6 +9,12 @@ import (
 )
 
 func connect(ctx context.Context, d *plugin.QueryData) (*airtable.Client, error) {
+	// get airtable client from cache
+	cacheKey := "airtable"
+	if cachedData, ok := d.ConnectionManager.Cache.Get(cacheKey); ok {
+		return cachedData.(*airtable.Client), nil
+	}
+
 	airtableConfig := GetConfig(d.Connection)
 
 	if &airtableConfig == nil {
@@ -31,5 +37,10 @@ func connect(ctx context.Context, d *plugin.QueryData) (*airtable.Client, error)
 		return nil, errors.New("'databaseid' must be set in the connection configuration. Edit your connection configuration file and then restart Steampipe")
 	}
 
-	return airtable.NewClient(token), nil
+	client := airtable.NewClient(token)
+
+	// Save to cache
+	d.ConnectionManager.Cache.Set(cacheKey, client)
+
+	return client, nil
 }

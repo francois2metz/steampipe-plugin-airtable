3a9b59bac17e9d3f5dc9b8e998f51fd2c88f2b9b
François de Metz <francois@2metz.fr>
2021-10-17 18:00:29 +0200

    Add query column to filterByFormula.

--- airtable/table_airtable_table.go
+++ airtable/table_airtable_table.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
 )
 
 func tableAirtableTable(tableName string) *plugin.Table {
@@ -13,6 +14,7 @@ func tableAirtableTable(tableName string) *plugin.Table {
 		Description: "The " + tableName + " table.",
 		List: &plugin.ListConfig{
 			Hydrate: listTable(tableName),
+			KeyColumns: plugin.OptionalColumns([]string{"query"}),
 		},
 		Get: &plugin.GetConfig{
 			KeyColumns: plugin.SingleColumn("id"),
@@ -22,6 +24,7 @@ func tableAirtableTable(tableName string) *plugin.Table {
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "The record ID of the row."},
 			{Name: "created_time", Type: proto.ColumnType_TIMESTAMP, Description: "Time when the record was created."},
 			{Name: "fields", Type: proto.ColumnType_JSON, Description: "The fields of the row."},
+			{Name: "query", Type: proto.ColumnType_STRING, Description: "Filter string to [filterWithFormula](https://support.airtable.com/hc/en-us/articles/203255215).", Transform: transform.FromQual("query")},
 		},
 	}
 }
@@ -29,14 +32,20 @@ func tableAirtableTable(tableName string) *plugin.Table {
 func listTable(tableName string) func(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	return func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 		client, err := connect(ctx, d)
-		airtableConfig := GetConfig(d.Connection)
-		table := client.GetTable(*airtableConfig.DatabaseID, tableName)
 		if err != nil {
 			return nil, err
 		}
+		airtableConfig := GetConfig(d.Connection)
+		table := client.GetTable(*airtableConfig.DatabaseID, tableName)
 		offset := ""
+		queryFilter := ""
+
+		if d.KeyColumnQuals["query"] != nil {
+			queryFilter = d.KeyColumnQuals["query"].GetStringValue()
+		}
+
 		for {
-			records, err := table.GetRecords().WithOffset(offset).Do()
+			records, err := table.GetRecords().WithFilterFormula(queryFilter).WithOffset(offset).Do()
 			if err != nil {
 				if is404Error(err) {
 					return nil, nil
